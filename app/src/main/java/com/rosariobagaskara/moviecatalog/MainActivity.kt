9bc5026dc8f4573601cfb543f5d1b5b05cecc1be
Rosario Bagaskara <rosariobagaskara@gmail.com>
2022-08-31 17:27:21 +0700

    Init Commit

---
+++ app/src/main/java/com/rosariobagaskara/moviecatalog/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rosariobagaskara.moviecatalog
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
